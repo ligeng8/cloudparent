56ce7e8108d37c179edcc828552a55bff756284b
ligeng8 <36887805+ligeng8@users.noreply.github.com>
2018-08-17 16:44:01 +0800

    commit

--- euraka-server/src/main/java/com/dtmobile/spring/Application.java
+++ euraka-server/src/main/java/com/dtmobile/spring/Application.java
@@ -1,12 +1,14 @@
 package com.dtmobile.spring;
 
+import java.io.Serializable;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class Application {
+public class Application implements Serializable {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
